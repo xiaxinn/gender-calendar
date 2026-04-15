8058494eebf7cec8062fa55cf357ccc6386c0203
xiaxinn <2413752724@qq.com>
2026-04-15 20:41:04 +0800

    fix(移除字体): pcp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;600;700;900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </head>
       <body className="antialiased">
         {children}
